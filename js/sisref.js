82448cbc0df28cd08d9280bb22a906cb9c9467b9
Pedro Santos Guimarães <pedrosg@win.ufu.br>
2019-08-08 16:13:47 -0300

    Buscar valores na pagina do SISREF

--- js/sisref.js
+++ js/sisref.js
@@ -1,2 +1,12 @@
 document.body.style.background = "#aabbcc";
 console.log("Cor foi pra #abc");
+
+entrada = $("#registros").find(":contains('ENTRADA')").next('p').text();
+
+console.log(entrada);
+
+intervalo = $("#registros").find(":contains('INTERVALO')").next('p').text().trim();
+
+console.log(intervalo);
+
+console.log("saiu");
