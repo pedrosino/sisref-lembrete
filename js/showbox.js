dc83ccf9e924690545c2a8b23578a1acf19f8cc6
Pedro <pedrosino@gmail.com>
2019-08-13 22:00:27 -0300

    Atualiza mensagem, estilos e botao para o SISREF

--- js/showbox.js
+++ js/showbox.js
@@ -8,21 +8,37 @@ browser.runtime.onMessage.addListener(request => {
   console.log("Message from the background script:");
   console.log(request.greeting);
 
+  // Caixa de diálogo para exibir a mensagem
   var div = document.createElement('dialog');
   div.setAttribute('id', 'alarmeDialog');
   div.setAttribute('class', 'alarme');
   div.textContent = request.greeting;
-  var button = document.createElement('button');
-  button.setAttribute('id','cancel');
-  button.setAttribute('class', 'ok');
-  button.innerHTML = 'Close';
-  div.appendChild(button);
-  document.body.appendChild(div);
-  //document.body.prepend(div);
 
-  var cancelButton = document.getElementById('cancel');
+  // Botão para fechar
+  var fechar = document.createElement('button');
+  fechar.setAttribute('id','fechar');
+  fechar.setAttribute('class', 'fechar');
+  fechar.innerHTML = 'Fechar';
+
+  //<a href="btnlink.html" id="btn"></a>
+  // Botão para ir para o site do sisref
+  var link = document.createElement('a');
+  link.setAttribute('id','linkSisref');
+  link.setAttribute('class','botaoLink');
+  link.href = "https://sisref.sigepe.gov.br/sisref/entrada.php";
+  link.setAttribute('target','_blank');
+  link.innerHTML = 'Ir para o SISREF';
+
+
+  div.appendChild(link);
+  div.appendChild(fechar);
+  //document.body.appendChild(div);
+  document.body.prepend(div);
+
+  var fecharButton = document.getElementById('fechar');
+  var linkButton = document.getElementById('linkSisref');
   var dialog = document.getElementById('alarmeDialog');
-  dialog.returnValue = 'favAnimal';
+  dialog.returnValue = 'sisref';
 
   dialogPolyfill.registerDialog(dialog);
 
@@ -36,8 +52,14 @@ browser.runtime.onMessage.addListener(request => {
 
   dialog.showModal();
 
-  // Form cancel button closes the dialog box
-  cancelButton.addEventListener('click', function() {
+  // Botão "Fechar" fecha a caixa
+  fecharButton.addEventListener('click', function() {
+    dialog.close('fechou');
+    openCheck(dialog);
+  });
+
+  // Botão "Link" também fecha a caixa
+  linkButton.addEventListener('click', function() {
     dialog.close('fechou');
     openCheck(dialog);
   });
